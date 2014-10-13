b1b5dc55feab4e140638e75f4922e8c5b12f4eaa
jattenberg <jattenberg@etsy.com>
2014-10-13 20:24:43 +0000

    adding fixes for comments

--- src/main/scala/com/etsy/conjecture/scalding/factorize/FactorizationTools.scala
+++ src/main/scala/com/etsy/conjecture/scalding/factorize/FactorizationTools.scala
@@ -3,43 +3,53 @@ package com.etsy.conjecture.scalding.factorize
 import cascading.pipe.Pipe
 import org.apache.commons.math3.linear._
 
+
+
 object FactorizationTools {
 
   def approxLeftFactorsLeastSquares(rightFactors : Pipe, id_sym : Symbol, right_vec_sym : Symbol,
                                     designMatrix : Pipe, left_id : Symbol, right_id : Symbol,
-                                    left_vec_symbol : Symbol)
-                                   (spill_threshold : Int = 1000000, parallelism : Int = 1000) : Pipe = {
+                                    left_vec_symbol : Symbol,
+                                    spill_threshold : Int = 1000000, parallelism : Int = 1000) : Pipe = {
 
     import com.twitter.scalding.Dsl._
-    val inv_sym = Symbol(right_vec_sym.name + "_inverse")
+    approxWeightLeftFactorsLeastSquares(rightFactors, id_sym, right_vec_sym,
+                                        designMatrix.insert('weight, 1.0), left_id, right_id,
+                                        'weight, left_vec_symbol, spill_threshold, parallelism)
+  }
+
+  def approxWeightLeftFactorsLeastSquares(rightFactors : Pipe, id_sym : Symbol, right_vec_sym : Symbol,
+                                          designMatrix : Pipe, left_id : Symbol, right_id : Symbol,
+                                          weight_symbol : Symbol, left_vec_symbol : Symbol,
+                                          spill_threshold : Int = 1000000, parallelism : Int = 1000) : Pipe = {
+
+    import com.twitter.scalding.Dsl._
+    val inv_sym = 'inverse
 
     val inv_self_outer = rightFactors
       .mapTo(right_vec_sym -> right_vec_sym) {
         l : RealVector => l.outerProduct(l)
       }
-      .groupAll{ _.reduce[RealMatrix](right_vec_sym){ (acc, x) => acc.add(x) } }
+      .groupAll{ _.reduce[RealMatrix](right_vec_sym){ (x, y) => x.add(y) } }
       .mapTo(right_vec_sym -> inv_sym) {
         ll : RealMatrix => new LUDecomposition(ll).getSolver.getInverse
       }
 
-    val weighted_right_factors = rightFactors
+    val premultiplied_right_factors = rightFactors
       .crossWithTiny(inv_self_outer)
-      .map((right_vec_sym, inv_sym) -> right_vec_sym) {
-        x:(RealVector, RealMatrix) => x._2.operate(x._1)
+      .map((right_vec_sym, inv_sym, weight_symbol) -> right_vec_sym) {
+        x:(RealVector, RealMatrix, Double) => x._2.operate(x._1.mapMultiply(x._3))
       }
       .project(id_sym, right_vec_sym)
 
 
-    val left_vectors = designMatrix.joinWithSmaller(right_id -> id_sym, weighted_right_factors)
+    designMatrix.joinWithSmaller(right_id -> id_sym, premultiplied_right_factors)
       .groupBy(left_id) {
-        _.reduce[RealVector](right_vec_sym -> left_vec_symbol){ (acc, x) => acc.add(x) }
+        _.reduce[RealVector](right_vec_sym -> left_vec_symbol){ (x, y) => x.add(y) }
          .reducers(parallelism)
          .spillThreshold(spill_threshold)
       }
-
       .project(left_id, left_vec_symbol)
-
-    left_vectors
   }
 
 }
